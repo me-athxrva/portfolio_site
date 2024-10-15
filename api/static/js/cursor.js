e5b8f0bb599181236de5695db0b3ea2105311d92
atharva-4 <atharvadeore04@gmail.com>
2024-10-15 23:58:44 +0530

    new upadted cursor function

--- api/static/js/cursor.js
+++ api/static/js/cursor.js
@@ -1,19 +1,23 @@
 var cursor = document.querySelector('.cursor');
 var nav_a = document.querySelectorAll('.nav_li a');
+var socials_a = document.querySelectorAll('.socials a');
 var a = document.querySelectorAll('a');
+var footer = document.querySelectorAll('footer');
 var img = document.querySelector('.cursor img');
 var clickme = document.querySelector('.cursor p');
 
-function set_cursor_atr(name,src){
+function set_cursor_atr(name,src,on_leave){
     // let src_2 = String.toString(src);
     for(let i=0;  i<name.length; i++){
         name[i].setAttribute("onmouseenter",src);
-        name[i].setAttribute("onmouseleave","remove_img()");
+        name[i].setAttribute("onmouseleave",on_leave);
     }
 }
 
-set_cursor_atr(a,"set_img('','expand')");
-set_cursor_atr(nav_a,"set_img('','contract')");
+set_cursor_atr(a,"set_img('','contract')","remove_img('black')");
+set_cursor_atr(nav_a,"set_img('','contract')","remove_img('black')");
+set_cursor_atr(footer, "set_img('','color_change')","rev_color('black')");
+set_cursor_atr(socials_a, "set_img('','contract')","remove_img('white')");
 
 
 window.addEventListener("mousemove", function(e){
@@ -32,41 +36,52 @@ function set_img(source,type){
         img.animate({
             opacity: 1},
             {duration: 300, fill: "forwards"});
-        cursor.animate({
-            backgroundColor: 'white'},
-            {duration: 300, fill: "forwards"});
-        clickme.animate({
-            opacity: 1,
-            color: 'black'},
-        {duration: 300, fill: "forwards", delay: 200})
+        setTimeout(() => {
+            clickme.animate({
+                opacity: 1,
+                color: 'white'},
+                {duration: 100, fill: "forwards"})
+        }, 200);
     }, 100);
-    if (type=='expand') {
-        cursor.animate({
-            height: '60px',
-            width: '60px'},
-            {duration: 200, fill: "forwards"});
-    } else if (type='contract') {
+    if (type=='contract') {
         cursor.animate({
             height: '0px',
             width: '0px'},
             {duration: 200, fill: "forwards"});
-    }
+    } else if (type=='color_change') {
+        cursor.animate({
+            backgroundColor: 'white'},
+            {duration: 200, fill: "forwards"});
+    };
 };
 
-function remove_img(){
+function rev_color(clr){
+    cursor.animate({
+        backgroundColor: clr},
+        {duration: 200, fill: "forwards", delay: 100});
+}
+
+function remove_img(clr){
     console.log("event recorded");
     img.src = "";
     setTimeout(() => {
         img.animate({
             opacity: 0},
             {duration: 300, fill: "forwards"});
-        
     }, 100);
-    cursor.animate({
-        backgroundColor: "red",
-        height: '20px',
-        width: '20px'},
-        {duration: 200, fill: "forwards", delay: 100});
+    if (clr=='white') {
+        cursor.animate({
+            backgroundColor: "white",
+            height: '20px',
+            width: '20px'},
+            {duration: 200, fill: "forwards", delay: 100});
+    } else if(clr=='black'){
+        cursor.animate({
+            backgroundColor: "black",
+            height: '20px',
+            width: '20px'},
+            {duration: 200, fill: "forwards", delay: 100});
+    }
     clickme.animate({
         opacity: 0,
         color: 'red'},
